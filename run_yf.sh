b9989aaccb23343743b44317b5a24a3230b3c6bb
Jian Zhang <zhangjianthu@gmail.com>
2017-11-20 09:10:00 -0800

    180 epoch running script

--- run_yf.sh
+++ run_yf.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python train.py data-bin/iwslt14.tokenized.de-en \
+CUDA_VISIBLE_DEVICES=0 python train.py data-bin/iwslt14.tokenized.de-en.14000 \
   --lr 0.25 --clip-norm 100000.0 --dropout 0.2 --max-tokens 4000 \
-  --arch fconv_iwslt_de_en --save-dir checkpoints/YF_final_test --use_YF --max-epoch=120 --lr-fac=1.0 
+  --arch fconv_iwslt_de_en --save-dir checkpoints/YF_no_drop_fac_1.0_180_epoch --use_YF --max-epoch=180 --lr-fac=1.0 
